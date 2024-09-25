53c4d9ce0d8d4695778e678feb26aa51117ae616
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2024-09-25 00:40:24 -0500

    feat: add box generator
    - BREAKING CHANGE: added an event listener that generates a grid based on any number up to 100.
    - changed the btn launch button to a function and removed the previous loop because both were no longer relevant/neeeded.

--- index.js
+++ index.js
@@ -2,81 +2,66 @@ const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
 const promptBtn = document.querySelector(".prompt.btn");
 
-const rows = `<div class='row'></div>
-                <div class='boxes'></div>`;
-
-// const boxRows = [
-//         `
-//         <div class="row one">
-//           <div class="square 1"></div>
-//           <div class="square 2"></div>
-//           <div class="square 3"></div>
-//           <div class="square 4"></div>
-//         </div>`,
-//         `
-//         <div class="row two">
-//           <div class="square 5"></div>
-//           <div class="square 6"></div>
-//           <div class="square 7"></div>
-//           <div class="square 8"></div>
-//         </div>`,
-//         `
-//         <div class="row three">
-//           <div class="square 9"></div>
-//           <div class="square 10"></div>
-//           <div class="square 11"></div>
-//           <div class="square 12"></div>
-//         </div>`,
-//         `
-//         <div class="row four">
-//           <div class="square 13"></div>
-//           <div class="square 14"></div>
-//           <div class="square 15"></div>
-//           <div class="square 16"></div>
-//         </div>`
-
-// ];
+const rows = `<div class='row'></div>`;
+const boxes = `<div class='boxes></div>`;
 
 const boxRows = [];
 
 
-// btn.addEventListener ('click', function event () {
+ function getStyles () {
 
-//         for (let i=0; i < boxRows.length; i++) {
-//                 container.innerHTML += boxRows[i];
-//         }
+        const squares = document.querySelectorAll(".boxes");
 
-//         const squares = document.querySelectorAll(".square");
+        squares.forEach(square => {
 
-//         squares.forEach(square => {
-
-//             square.addEventListener('mouseenter', function() {
-//             this.style.backgroundColor = 'green'
-//         });
+            square.addEventListener('mouseenter', function() {
+            this.style.backgroundColor = 'green'
+        });
     
-//             square.addEventListener('mouseleave', function() {
-//             this.style.backgroundColor = 'blue'
-//         }); 
+            square.addEventListener('mouseleave', function() {
+            this.style.backgroundColor = 'blue'
+        }); 
 
-//      });
-// });
+     });
+};
+
+
 // Might have to separate the mouse in/out event listeners into their own function later.
-promptBtn.addEventListener('click', function () {
-        let input = Number(prompt('How many boxes per side would you like?'));
-        
 
-        if (input < 100) {
-          for (let i = 0; i < input; i++) {
-              boxRows.push(rows);
-          }
-          boxRows.forEach(row => {
-            container.innerHTML += row;
-          });
-          
-        } else if (input > 100) {
-                alert("Unit maximum is 100")
-                return;
-        }
-})
-// .for each
-// .length vs .value
+
+promptBtn.addEventListener('click', function () {
+
+  let input = Number(prompt('How many boxes per side would you like?'));
+
+  if (input < 100) {
+
+    container.innerHTML = '';
+
+    for (let i = 0; i < input; i++) {
+
+      let row = document.createElement('div');
+      row.className = 'rows';
+      container.appendChild(row);
+      boxRows.push(row); 
+
+    } 
+
+      boxRows.forEach(row => { 
+
+        for (let i = 0; i < input; i++) {
+
+          let box = document.createElement('div');
+          box.className = 'boxes';
+          row.appendChild(box);
+
+      }
+
+    });
+
+      getStyles();
+
+  } else if (input > 100) {
+          alert("Unit maximum is 100")
+          return;
+  }
+});
